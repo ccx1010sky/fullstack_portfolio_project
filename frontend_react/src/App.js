b7145c5c0c2fcb9ede3b1440fc5524ce93bb3326
ccx1010sky <ccx1010@gmail.com>
2022-09-06 19:52:36 +0100

    npm install sass,instead of node-sass

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 import { About, Footer, Skills, Header, Work } from "./container";
+import {Navbar} from "./components"
 
 const App = () => {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <About />
       <Skills />
       <Work />
       <Footer />
+
+
     </div>
   );
 };
