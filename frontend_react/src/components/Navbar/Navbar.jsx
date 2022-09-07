a7c0afb74e35c31384f72d523e6492d39dc85dc9
ccx1010sky <ccx1010@gmail.com>
2022-09-07 14:27:50 +0100

    motion.div

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,56 @@
-import React from 'react'
-import "./Navbar.scss";
-import { images } from "../../constants"
+import React, { useState } from "react";
 
-const arr = ["home","about","skills","work","contact"]
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+
+import "./Navbar.scss";
+import { images } from "../../constants";
+
+const arr = ["home", "about", "skills", "work", "contact"];
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
+    <nav className="app__navbar">
+      {/* 1,logo */}
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
-      <ul className='app__navbar-links'>
-        {arr.map(item => (
-          <li className='app__flex p-text' key= {`link-${item}`}>
+
+      {/* 2,links */}
+      <ul className="app__navbar-links">
+        {arr.map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
             <div />
-            <a href= { `#${item}`}>{item}</a>
+            <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
+
+      {/* 3, menu */}
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [400, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {arr.map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
