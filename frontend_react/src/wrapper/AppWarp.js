bdeae8ff62bbd71a4aa3b491d3ca153ba7396b92
ccx1010sky <ccx1010@gmail.com>
2022-10-02 20:59:56 +0100

    create a AppWarp for higher order component

--- frontend_react/src/wrapper/AppWarp.js
+++ frontend_react/src/wrapper/AppWarp.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from "react";
+import NavigationDots from "../components/NavigationDots";
+import SocialMedia from "../components/SocialMedia";
 
-
-const AppWarp = () => {
+//Higher Order Component
+const AppWarp = (component, idName, classNames) => {
   return (
-    <div>AppWarp</div>
-  )
-}
+    <div id={idName} className={`app__container ${classNames}`}>
+      <SocialMedia />
 
-export default AppWarp
+      <div className="app__wrapper  app_flex">
+        <component />
+        <div className="copyright">
+          <p className="p-text">@ 2022 Nick</p>
+          <p className="p-text">All Right Reserve</p>
+        </div>
+      </div>
+      <NavigationDots />
+    </div>
+  );
+};
+
+export default AppWarp;
