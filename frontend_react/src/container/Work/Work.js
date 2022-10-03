228204272440809bd12cb8a3f80a5c6e3d86b211
ccx1010sky <ccx1010@gmail.com>
2022-10-03 22:07:13 +0100

    client fetch data from works schema

--- frontend_react/src/container/Work/Work.js
+++ frontend_react/src/container/Work/Work.js
@@ -1,27 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 import { useFor, client } from "../../client";
 import AppWrap from "../../wrapper/AppWrap";
 import "./Work.scss";
-const Work = () => {
-  //  const [works, setWorks] = useState([]);
-  //  const [filterWork, setFilterWork] = useState([]);
-  //  const [activeFilter, setActiveFilter] = useState("All");
 
-  const works = ["UI/UX", "Web App", "Mobile App", "ReactJS", "All"];
+
+
+const Work = () => {
+
+   const [works, setWorks] = useState([]);
+   const [filterWork, setFilterWork] = useState([]);
+   const [activeFilter, setActiveFilter] = useState("All");
+
+  useEffect(() => {
+    const query= '*[_type = "works"]'
+    client.fetch(query)
+    .then(
+      (data)=>{
+      setWorks(data) 
+      setFilterWork(data)}
+    )
+    
+  }, [])
+  
+  const worksArr = ["UI/UX", "Web App", "Mobile App", "ReactJS", "All"];
 
   const handleWorkFilter = (item) => {
-    // setActiveFilter(item);
+    setActiveFilter(item);
   };
+
+
+
+
 
   return (
     <div className="app__work app__flex">
       <h2>
         My Creative <span>Portfolio</span> Section
       </h2>
-      <div className="app__work-fileter">
-        {works.map((item, index) => (
+      <div className="app__work-filter">
+        {worksArr.map((item, index) => (
           <div
             className="app__work-filter-item  app__flex"
             key={item + index}
