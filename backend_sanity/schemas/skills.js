ecae28fcbb0ab11f31ab4f2c8b931d88b2ae8ff0
ccx1010sky <ccx1010@gmail.com>
2022-10-08 12:12:26 +0100

    create schema  skills.js experiences.js etc

---
+++ backend_sanity/schemas/skills.js
@@ -0,0 +1,27 @@
+export default {
+  name: "skills",
+  title: "Skills",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+
+    {
+      name: "bgColor",
+      title: "BgColor",
+      type: "string",
+    },
+
+    {
+        name: "icon",
+        title:"Icon",
+        type:'image',
+        options:{
+            hotspot:true,
+        },
+    },
+]
+}
