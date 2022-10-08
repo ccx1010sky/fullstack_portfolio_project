14476541a23874658f94ce189b5c6205354de29b
ccx1010sky <ccx1010@gmail.com>
2022-10-08 13:13:35 +0100

    skills.js to display the page

--- frontend_react/src/container/Skills/Skills.js
+++ frontend_react/src/container/Skills/Skills.js
@@ -1,9 +1,46 @@
-import React from 'react'
-import "./Skills.scss"
-const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
-}
+import React,{ useState, useEffect} from "react";
+import "./Skills.scss";
+import { urlFor, client } from "../../client";
 
-export default Skills
+const Skills = () => {
+  const [skills, setSkills] = useState([]);
+  const [experiences, setExperiences] = useState([]);
+
+  useEffect(() => {
+    const skillsQuery = "*[_type == 'skills']";
+    const query = " *[_type == 'experiences']";
+    client.fetch(query).then((data) => setSkills(data));
+
+    client.fetch(query).then((data) => setExperiences(data));
+  }, []);
+
+  return (
+    <div className="app__skills">
+      <h2 className="head-text"> Work & Experience</h2>
+
+      <div className="app__skills-container app__flex">
+        <div className="app__skills-list app__flex">
+          {skills.map((skill,index)=>
+          (
+            <div className="app__skill-item app__flex">
+              <div
+                className="app__flex}"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </div>
+          )
+          )}
+        </div>
+
+        <div className="app__skills-experience app__flex">
+          year and role comapany
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
