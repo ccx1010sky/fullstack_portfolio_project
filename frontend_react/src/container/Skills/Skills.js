9a818a61f2d38cec1997877aa353efb45f8cd836
ccx1010sky <ccx1010@gmail.com>
2022-10-10 13:56:52 +0100

    skills.js error

--- frontend_react/src/container/Skills/Skills.js
+++ frontend_react/src/container/Skills/Skills.js
@@ -1,37 +1,41 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
-import "./Skills.scss";
+import { motion } from "framer-motion";
+import ReactTooltip from "react-tooltip";
+
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
+import "./Skills.scss";
 
 const Skills = () => {
-  const [skills, setSkills] = useState([]);
   const [experiences, setExperiences] = useState([]);
+  const [skills, setSkills] = useState([]);
 
   useEffect(() => {
-    const query = " *[_type == 'experiences']";
-    const skillsQuery = "*[_type == 'skills']";
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
 
-    client.fetch(query).then((data) => setExperiences(data));
+    client.fetch(query).then((data) => {
+      setExperiences(data);
+    });
+
     client.fetch(skillsQuery).then((data) => {
       setSkills(data);
     });
   }, []);
 
-=======
-import React from 'react'
-import "./Skills.scss"
-const Skills = () => {
->>>>>>> parent of 14476541 (skills.js to display the page)
   return (
-    <div>Skills</div>
-  )
-}
+    <>
+      <h2 className="head-text">Skills & Experiences</h2>
 
-<<<<<<< HEAD
       <div className="app__skills-container">
-        <div className="app__skills-list app__flex">
-          {skills.map((skill, index) => (
-            <div className="app__skill-item app__flex">
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
               <div
                 className="app__flex"
                 style={{ backgroundColor: skill.bgColor }}
@@ -39,25 +43,50 @@ const Skills = () => {
                 <img src={urlFor(skill.icon)} alt={skill.name} />
               </div>
               <p className="p-text">{skill.name}</p>
-            </div>
+            </motion.div>
           ))}
-        </div>
-
-        <div className="app__skills-experience app__flex">
-          
-          {experiences.map((experience, index) => (
-            <div>
-
-              <p>{experience.year}</p>
-            </div>
+        </motion.div>
+        <div className="app__skills-exp">
+          {experiences.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience.year}>
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
+                ))}
+              </motion.div>
+            </motion.div>
           ))}
         </div>
       </div>
-    </div>
+    </>
   );
 };
-          
-export default Skills;
-=======
-export default Skills
->>>>>>> parent of 14476541 (skills.js to display the page)
+
+export default AppWrap(
+  MotionWrap(Skills, "app__skills"),
+  "skills",
+  "app__whitebg"
+);
